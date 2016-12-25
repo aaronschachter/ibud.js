87303b636301f253d7b2916265d024c942ab4371
Aaron Schachter <aaronschachter@gmail.com>
2016-12-25 12:29:38 -0800

    Adds dont_know postback

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
   var data = req.body;
-  console.log(data);
 
   // Make sure this is a page subscription
   if (data.object == 'page') {
@@ -100,46 +99,35 @@ function callSendAPI(messageData) {
 }
 
 function receivedMessage(event) {
+  console.log('event');
   console.log(event);
 
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var timeOfMessage = event.timestamp;
   var message = event.message;
-
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
-
-  var isEcho = message.is_echo;
-  var messageId = message.mid;
-  var appId = message.app_id;
-  var metadata = message.metadata;
-
-  // You may get a text or attachment but not both
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-  var quickReply = message.quick_reply;
-
-  if (isEcho) {
-    // Just logging message echoes to console
-    console.log("Received echo for message %s and app %d with metadata %s", 
-      messageId, appId, metadata);
-    return;
-  } else if (quickReply) {
-    var quickReplyPayload = quickReply.payload;
-    console.log("Quick reply for message %s with payload %s",
-      messageId, quickReplyPayload);
-
-    sendTextMessage(senderID, "Quick reply tapped");
-    return;
+  if (message) {
+    // You may get a text or attachment but not both
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+    var quickReply = message.quick_reply;
   }
 
   if (messageText) {
-    sendTextMessage(senderID, messageText);
+    // TODO: Check for commands like Hi, Hello, Help, Quit, Cancel, Who is this / Who are you
+    sendInterviewQuestion(senderID, messageText);
+  } else if (event.postback && event.postback.payload === 'dont_know') {
+    console.log('dont_know');
+    sendInterviewQuestion(senderID, messageText);
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
   }
+}
+
+function getQuestion() {
+  const index = Math.floor(Math.random() * (app.locals.questions.length + 1));
+
+  return app.locals.questions[index];
 }
 
 /*
@@ -162,6 +150,32 @@ function sendTextMessage(recipientId, messageText) {
     }
   };
 
+  callSendAPI(messageData);
+}
+
+function sendInterviewQuestion(recipientId, questionText) {
+
+  const messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'button',
+          text: getQuestion(),
+          buttons: [
+            {
+              type: 'postback',
+              title: 'I don\'t know',
+              payload: 'dont_know',
+            },
+          ],
+        },
+      },
+    },
+  };
   callSendAPI(messageData);
 }
 
